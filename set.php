09b8b5924ffb26f54d7844e7c57ee41f258f649d
thundr-inc <107603214+thundr-inc@users.noreply.github.com>
2022-07-07 10:34:03 +0530

    Add files via upload

---
+++ set.php
@@ -0,0 +1,23 @@
+<?php
+///////////////////////////////////////////////////////
+///////// KAGAK USAH DELET CREDIT AJG /////////////////
+///////////////////////////////////////////////////////
+
+# [Creator] Clownxx
+# [Instagram] _clownxx
+# [Discord] TCMC#5627
+# [Group Discord] https://discord.gg/hNb6aUmHzk
+
+//masukan username akun asli yang akan
+//di add followers
+$user="xxxxxx";
+
+//masukan username akun tumbal kalian
+$t_user="xxxxxx";
+
+//masukan passwoard akun tumbal
+$t_pass="xxxxxx";
+
+//masuk y, untuk auto running script
+//masukan n, untuk tidak aktivkan auto running script
+$ulang="xxxxxxx";
